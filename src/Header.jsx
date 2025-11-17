c555107b3cecb257fbbe8acbe8f3cab0f718ca97
beom <qjagkrdldi@gmail.com>
2025-11-17 09:36:00 +0900

    버튼명 수정

--- src/Header.jsx
+++ src/Header.jsx
@@ -36,7 +36,7 @@ function Header() {
           <FaBars className="md:hidden" />
         </button>
         <button className="hidden md:block bg-blue-500 hover:bg-blue-600 text-white font-bold py-1.5 px-4 rounded transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          강의
+          sss
         </button>
       </div>
 
